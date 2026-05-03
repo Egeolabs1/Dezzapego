d91ac94663f49371f6b6acce62ca1b301c71843e
sickboy81 <ngfilho@gmail.com>
2026-05-03 11:51:54 -0300

    Fix: add missing useEffect import in Home.tsx

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import { formatPrice } from '../../lib/formatters';
 
 import { useSearchParams } from 'react-router-dom';
